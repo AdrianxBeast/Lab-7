c09a3c258013a93df31b0e70783768bae821684b
AdrianxBeast <136941434+AdrianxBeast@users.noreply.github.com>
2024-03-08 21:03:12 -0500

    Add files via upload

---
+++ lab7.c
@@ -0,0 +1,82 @@
+//Adrian Salgado COP3223
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+typedef struct {
+    int value;
+    int swapCount;
+} nums;
+
+void printSwapCount(nums arr[], int n) {
+    int i;
+    for (i = 0; i < n; i++) {
+        printf("%d: %d\n", arr[i].value, arr[i].swapCount);
+    }
+}
+
+void bubbleSort(nums arr[], int n) {
+    int i, j, totalSwaps = 0;
+    nums temp;
+//bubble sort
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j].value > arr[j + 1].value) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                arr[j].swapCount++;
+                arr[j + 1].swapCount++;
+                totalSwaps++;
+            }
+        }
+    }
+
+    printf("Bubble Sort - Total swaps: %d\n", totalSwaps);
+    printSwapCount(arr, n);
+}
+
+void selectionSort(nums arr[], int n) {
+    int i, j, min_idx, totalSwaps = 0;
+    nums temp;
+
+    for (i = 0; i < n - 1; i++) {
+        min_idx = i;
+        for (j = i + 1; j < n; j++) {
+            if (arr[j].value < arr[min_idx].value)
+                min_idx = j;
+        }
+        if (min_idx != i) {
+            temp = arr[i];
+            arr[i] = arr[min_idx];
+            arr[min_idx] = temp;
+            arr[i].swapCount++;
+            arr[min_idx].swapCount++;
+            totalSwaps++;
+        }
+    }
+
+    printf("Selection Sort - Total swaps: %d\n", totalSwaps);
+    printSwapCount(arr, n);
+}
+
+int main() {
+    nums array1[9] = {{97, 0}, {16, 0}, {45, 0}, {63, 0}, {13, 0}, {22, 0}, {7, 0}, {58, 0}, {72, 0}};
+    nums array2[9] = {{90, 0}, {80, 0}, {70, 0}, {60, 0}, {50, 0}, {40, 0}, {30, 0}, {20, 0}, {10, 0}};
+    nums array1_copy[9], array2_copy[9];
+
+    // Copy the original arrays
+    memcpy(array1_copy, array1, sizeof(array1));
+    memcpy(array2_copy, array2, sizeof(array2));
+
+    printf("Bubble sort Array 1: ");
+    bubbleSort(array1, 9);
+    printf("\nBubble sort Array 2: ");
+    bubbleSort(array2, 9);
+
+    printf("\nSelection sort Array 1: ");
+    selectionSort(array1_copy, 9);
+    printf("\nSelection sort Array 2: ");
+    selectionSort(array2_copy, 9);
+
+    return 0;
+}
